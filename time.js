683c4d6355b94a1b928f384d68586aaf6324bc72
lhgzsmc <67996483+lhgzsmc@users.noreply.github.com>
2020-07-13 14:20:21 +0800

    Create time.js

---
+++ time.js
@@ -0,0 +1,31 @@
+let now = new Date();
+
+let year = now.getFullYear();
+
+let month = now.getMonth() + 1;
+
+let day = now.getDate();
+
+let hour = now.getHours();
+
+let minute = now.getMinutes();
+
+let second = now.getSeconds();
+
+let dayOfWeek = now.getDay();
+
+
+
+console.log(year);
+
+console.log(month);
+
+console.log(day);
+
+console.log(hour);
+
+console.log(minute);
+
+console.log(second);
+
+console.log(dayOfWeek);
