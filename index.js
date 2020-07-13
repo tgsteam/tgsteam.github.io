2b033dbea40430965a2111bc3bd4de0d4e8df78f
lhgzsmc <67996483+lhgzsmc@users.noreply.github.com>
2020-07-13 14:21:11 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,459 @@
+<script src="scripts/main.js" defer></script><html lang="en" itemscope itemtype="http://schema.org/WebPage">
+ <head></head>
+ <body class="sticky-enable home-1">
+  ﻿   
+  <meta charset="UTF-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <title>莱蒙庄园官方网站</title> 
+  <link rel="shortcut icon" href="icon/lemon2.ico" type="image/x-icon"> 
+  <link href="assets/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+  <!-- 标题css引用--> 
+  <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css"> 
+  <link rel="stylesheet" href="assets/vendors/slick/slick.min.css"> 
+  <link rel="stylesheet" href="assets/vendors/slick/slick-theme.min.css"> 
+  <link rel="stylesheet" href="assets/vendors/animate.min.css"> 
+  <link rel="stylesheet" href="assets/vendors/magnific-popup/magnific-popup.css"> 
+  <link rel="stylesheet" href="assets/vendors/bootstrap-datepicker/css/bootstrap-datepicker.min.css"> 
+  <!-- ico图标--> 
+  <link reL="stylesheet" href="assets/fonts/font-icon/style.css"> 
+  <!--  真正的css链接--> 
+  <link rel="stylesheet" type="text/css" href="assets/css/layout.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/elements.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/shortcode.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/widget.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/woocommerce.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/extra.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/responsive.css"> 
+  <!-- 谷歌引用css--> 
+  <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Oswald:300,400" rel="stylesheet">   
+  <div id="pagewrap" class="pagewrap"> 
+   <div id="html-content" class="wrapper-content"> 
+    <header class="header"> 
+     <div class="header-main"> 
+      <div class="container"> 
+       <div class="open-offcanvas">
+        ☰
+       </div> 
+       <div class="utility-nav"> 
+        <div class="dropdown">
+         <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="search-bar dropdown-toggle"><i class="fa fa-search"></i></a> 
+         <div class="dropdown-menu dropdown-menu-right"> 
+          <div class="search-form"> 
+           <form role="search" method="get" action="index.html" class="search-form"> 
+            <input type="text" placeholder="Search... " name="s" class="searchbox"> 
+            <button type="submit" class="search-btn fa fa-search"></button> 
+           </form> 
+          </div> 
+         </div> 
+        </div> 
+       </div> 
+       <!-- 上层菜单图标logo--> 
+       <div class="header-logo">
+        <a href="index.html" class="logo logo-text"><img src="assets/images/logo.png" alt="莱蒙庄园" class="logo-sticky"><img src="assets/images/logo.png" alt="莱蒙庄园" class="logo-trans"></a>
+       </div> 
+       <!-- 主菜单--> 
+       <nav id="main-nav-offcanvas" class="main-nav-wrapper"> 
+        <div class="close-offcanvas-wrapper">
+         <span class="close-offcanvas">x</span>
+        </div> 
+        <div class="main-nav"> 
+         <ul id="main-nav" class="nav nav-pills"> 
+          <li class="menu-item nav-item"><a href="index.html" class="main-text nav-link">主页</a></li> 
+          <li class="menu-item nav-item"><a href="download.html" class="main-text nav-link">下载客户端</a></li> 
+          <li class="menu-item nav-item"><a href="login.html" class="main-text nav-link">注册（毛用没有）</a></li> 
+         </ul> 
+        </div> 
+       </nav> 
+      </div> 
+     </div> 
+    </header> 
+    <div class="page-container"> 
+     <!-- top slider--> 
+     <div class="top-slider sliders style-01"> 
+      <div role="toolbar" class="slides"> 
+       <div class="slide-item"> 
+        <div class="content"> 
+         <div> 
+          <div data-ani-in="swinFadeInRight" data-ani-out="fadeOut" data-ani-delay="0" class="main-title swin-font-oswald animated swinFadeInRight"> 
+           <div class="large-text">
+            莱蒙庄园
+           </div> 
+           <div class="small-text">
+            / 一个小小的Minecraft服务器
+           </div> 
+          </div> 
+          <div data-ani-in="swinFadeInRight" data-ani-out="fadeOut" data-ani-delay="400" class="sub-title swin-font-oswald animated swinFadeInRight"></div>
+          <a href="download.html" data-ani-in="swinFadeInRight" data-ani-out="fadeOut" data-ani-delay="800" tabindex="0" class="swin-btn text-left btn-transparent animated swinFadeInRight"><span>下载客户端</span></a> 
+         </div> 
+        </div> 
+        <div class="slide-bg">
+         <img src="image/background.jpg" alt="莱蒙庄园">
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+   <div class="page-content"> 
+    <!-- contact infor--> 
+    <section class="contact-section-simple"> 
+     <div class="container"> 
+      <div class="contact-wrap"> 
+       <div class="row"> 
+        <div class="col-md-4"> 
+         <div class="swin-sc swin-sc-infobox swin-sc-infobox-102453262 dark"> 
+          <h3 class="info-label">我们的服务器数量</h3> 
+          <p class="info-content"><span>截止至2020/7/5，共3个服务器</span></p> 
+         </div> 
+        </div> 
+        <div class="col-md-4"> 
+         <div class="swin-sc swin-sc-infobox swin-sc-infobox-115249653 dark"> 
+          <h3 class="info-label">腐竹联系方式</h3> 
+          <p class="info-content"><span>QQ:3175920643<br>e-mail:lemon8864@foxmail.com</span></p> 
+         </div> 
+        </div> 
+        <div class="col-md-4"> 
+         <div class="swin-sc swin-sc-social swin-sc-social-1776940560 dark"> 
+          <p class="label-social"><span><a href="">点我加入官方QQ交流群</a></span></p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </section> 
+    <!-- infor--> 
+    <section> 
+     <div class="container"> 
+      <div class="row"> 
+       <div class="col-md-6"> 
+        <div class="swin-sc swin-sc-title style-01 text-left dark"> 
+         <h3 class="main-title title dark">我们的服务器地址</h3> 
+         <div class="title-shape">
+          <span class="shape"></span>
+         </div> 
+        </div> 
+        <div class="text-description"> 
+         <p>主生存服：mc.laoduan.net，端口19132（版本1.14.60）<br>末日求生服：lemon.laoduan.net，端口19132（版本1.16.0）<br>原版生存服：bdx.laoduan.net，端口19132（版本1.16.0）</p> 
+        </div> 
+        <div class="swin-sc swin-btn-wrap text-left mb-5 mb-md-0">
+         <a href="doc/服务器介绍及进入方法.docx" class="swin-btn"><span>查看帮助文档</span></a>
+        </div> 
+       </div> 
+       <div class="col-md-6"> 
+        <div class="swin-sc swin-sc-image left"> 
+         <div class="wrap-img">
+          <img src="logo/logo.png" alt="莱蒙庄园" class="img img-responsive">
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </section> 
+    <!-- counter section--> 
+    <section> 
+     <div class="container"> 
+      <div class="swin-sc swin-sc-title style-01 text-center dark"> 
+       <h3 class="main-title title dark">我们的成就</h3> 
+       <p class="sub-title"><span>来看看一些数字吧</span></p> 
+       <div class="title-shape">
+        <span class="shape"></span>
+       </div> 
+      </div> 
+      <div class="col-md-8 offset-md-2"> 
+       <div class="text-description center">
+        服务器从2020/4开放以来的一些统计数据
+       </div> 
+      </div> 
+      <div class="swin-sc swin-sc-counter"> 
+       <div class="row"> 
+        <div class="col-lg-3 col-md-6"> 
+         <div class="counter-info item"> 
+          <p data-from="0" data-to="286" data-speed="3000" data-refresh-interval="5" class="number timer title">286</p> 
+          <p class="caption">总进入玩家数</p> 
+         </div> 
+        </div> 
+        <div class="col-lg-3 col-md-6"> 
+         <div class="counter-info item"> 
+          <p data-from="50" data-to="78" data-speed="3000" data-refresh-interval="5" class="number timer title">78</p> 
+          <p class="caption">总运行天数</p> 
+         </div> 
+        </div> 
+        <div class="col-lg-3 col-md-6"> 
+         <div class="counter-info item"> 
+          <p data-from="200" data-to="13" data-speed="3000" data-refresh-interval="5" class="number timer title">13</p> 
+          <p class="caption">最高在线玩家数</p> 
+         </div> 
+        </div> 
+        <div class="col-lg-3 col-md-6"> 
+         <div class="counter-info item"> 
+          <p data-from="450" data-to="688" data-speed="3000" data-refresh-interval="5" class="number timer title">688</p> 
+          <p class="caption">地图总大小（MB）</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </section> 
+    <!-- project section--> 
+    <section> 
+     <div class="container"> 
+      <div class="row"> 
+       <div class="col-md-6"> 
+        <div class="swin-sc swin-sc-title style-01 text-center dark"> 
+         <h3 class="main-title title dark">服务器风景</h3> 
+         <p class="sub-title"><span>我们独特的风景线</span></p> 
+         <div class="title-shape">
+          <span class="shape"></span>
+         </div> 
+        </div> 
+       </div> 
+       <div class="col-md-6"> 
+        <div class="text-description">
+         感谢广大玩家的支持与建设，才造就我们今天的美景，我们也欢迎新玩家加入，在服务器留下你的足迹。服务器地图定时备份并开放，且纯公益，0氪金
+        </div> 
+        <div class="swin-sc swin-btn-wrap text-left builday-mtop35"></div> 
+       </div> 
+      </div> 
+      <div class="swin-sc swin-sc-project-masonry builday-mtop35"> 
+       <div class="grid"> 
+        <div class="project-item item grid-item col-sm-6 grid-item-h1"> 
+         <div class="project-item-inner"> 
+          <div class="project-image">
+           <img src="assets/images/project/project01.png" alt="" class="img img-responsive swin-transition">
+          </div> 
+          <div class="project-content"> 
+           <div class="project-content-inner">
+            <a href="#" class="view-lightbox"><i class="fa fa-long-arrow-right"></i></a> 
+            <h4 class="project-heading"><a href="#">生存服-生存区</a></h4> 
+           </div> 
+          </div> 
+         </div> 
+        </div> 
+        <div class="project-item item grid-item col-sm-6 grid-item-h2"> 
+         <div class="project-item-inner"> 
+          <div class="project-image">
+           <img src="assets/images/project/project02.png" alt="" class="img img-responsive swin-transition">
+          </div> 
+          <div class="project-content"> 
+           <div class="project-content-inner">
+            <a href="#" class="view-lightbox"><i class="fa fa-long-arrow-right"></i></a> 
+            <h4 class="project-heading"><a href="#">生存服-生存区</a></h4> 
+           </div> 
+          </div> 
+         </div> 
+        </div> 
+        <div class="project-item item grid-item col-sm-3 grid-item-h1"> 
+         <div class="project-item-inner"> 
+          <div class="project-image">
+           <img src="assets/images/project/project03.png" alt="" class="img img-responsive swin-transition">
+          </div> 
+          <div class="project-content"> 
+           <div class="project-content-inner">
+            <a href="#" class="view-lightbox"><i class="fa fa-long-arrow-right"></i></a> 
+            <h4 class="project-heading"><a href="#">曾经的空岛服</a></h4> 
+           </div> 
+          </div> 
+         </div> 
+        </div> 
+        <div class="project-item item grid-item col-sm-3 grid-item-h1"> 
+         <div class="project-item-inner"> 
+          <div class="project-image">
+           <img src="assets/images/project/project04.jpg" alt="" class="img img-responsive swin-transition">
+          </div> 
+          <div class="project-content"> 
+           <div class="project-content-inner">
+            <a href="#" class="view-lightbox"><i class="fa fa-long-arrow-right"></i></a> 
+            <h4 class="project-heading"><a href="#">原版生存服-丛林聚落</a></h4> 
+           </div> 
+          </div> 
+         </div> 
+        </div> 
+        <div class="grid-sizer col-sm-1"></div> 
+       </div> 
+      </div> 
+     </div> 
+    </section> 
+    <!-- team section--> 
+    <section> 
+     <div class="container"> 
+      <div class="swin-sc swin-sc-title style-01 text-center dark"> 
+       <h3 class="main-title title dark">莱蒙庄园-核心团队</h3> 
+       <p class="sub-title"><span>筑造庄园的人们</span></p> 
+       <div class="title-shape">
+        <span class="shape"></span>
+       </div> 
+      </div> 
+      <div class="row"> 
+       <div class="col-md-8 offset-md-2"> 
+        <div class="text-description center">
+         从2020/4开始，这些人共同打造了如今的莱蒙庄园
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+     <div data-slidestoshow="5" class="swin-sc swin-sc-team-slider has_slider"> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-01.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">Lemon8864</p> 
+          <p class="team-position">小破腐竹</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-02.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">酸菜鱼</p> 
+          <p class="team-position">原版生存服前腐竹</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-03.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">鱼</p> 
+          <p class="team-position">老司机+pcr·fgo秃子</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-06.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">晓天</p> 
+          <p class="team-position">带白嫖怪</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-05.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">阿米娅</p> 
+          <p class="team-position">VCS ROOM腐竹，最可爱の小驴砸</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+      <div class="team-item swin-transition"> 
+       <div class="team-item-inner swin-transition"> 
+        <div class="team-img">
+         <img src="assets/images/team/team-07.jpg" alt="" class="img img-responsive">
+        </div> 
+        <div class="team-info swin-transition"> 
+         <div class="team-info-inner swin-transition"> 
+          <p class="team-name">星辰</p> 
+          <p class="team-position">老二次元了，绝对の死宅</p> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </section> 
+   </div> 
+  </div> 
+  <footer> 
+   <!-- footer main--> 
+   <!-- Start Footer Content--> 
+   <div class="footer-content"> 
+    <div class="container"> 
+     <div class="footer-top-section"> 
+      <div class="footer-logo">
+       <img src="assets/images/logo.png" alt="莱蒙庄园" class="footer-logo-img">
+      </div> 
+      <div class="footer-top-widget"> 
+       <div id="nav_menu-2" class="widget-sidebar swin-widget widget_nav_menu"> 
+        <div class="menu-footer-top-container"> 
+         <ul id="menu-footer-top" class="menu list-unstyled"> 
+          <li class="menu-item active"><a href="index.html">主页</a></li> 
+          <li class="menu-item"><a href="download.html">下载客户端</a></li> 
+          <li class="menu-item"><a href="login.html">注册账号（也是没卵用）</a></li>  
+         </ul> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+     <div class="footer-main"> 
+      <div class="row"> 
+       <div class="ft-widget-area ft-area1 col-lg-3 col-md-6 col-sm-12"> 
+        <div id="builday_core_icon_box-2" class="widget-sidebar swin-widget widget_icon_box"> 
+         <div class="widget-title">
+          联系方式
+         </div> 
+         <div class="widget-icon-box clearfix"> 
+          <div class="box-item"> 
+           <div class="icon">
+            <i class="fa fa-map-marker"></i>
+           </div> 
+           <div class="info-content">
+            腐竹：
+            <br>QQ:3175920643
+            <br>电子邮件：lemon8864@foxmail.com 
+           </div> 
+          </div> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> 
+    </div> 
+   </div>    
+   <!-- End Footer Content--> 
+   <!-- Footer bottom--> 
+   <div class="site-info footer-bottom"> 
+    <div class="container"> 
+     <div class="content-text">
+      <span class="text">2020 Lemon Manor/Lemon's Manor 版权所有</span>
+     </div> 
+    </div> 
+   </div> 
+  </footer>
+  <a id="totop" href="#" class="animated zoomIn"><i class="fa fa-angle-up"></i></a>   
+  <!-- jQuery--> 
+  <script src="assets/vendors/jquery-1.10.2.min.js"></script> 
+  <!-- Bootstrap Dependence--> 
+  <script src="assets/vendors/popper.min.js"></script> 
+  <!-- Bootstrap JavaScript--> 
+  <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script> 
+  <!-- Vendors--> 
+  <script src="assets/vendors/slick/slick.min.js"></script> 
+  <script src="assets/vendors/isotope/isotope.pkgd.min.js"></script> 
+  <script src="assets/vendors/jquery-countTo/jquery.countTo.min.js"></script> 
+  <script src="assets/vendors/jquery-appear/jquery.appear.min.js"></script> 
+  <script src="assets/vendors/parallax/parallax.min.js"></script> 
+  <script src="assets/vendors/gmaps/gmaps.min.js"></script> 
+  <script src="assets/vendors/audiojs/audio.min.js"></script> 
+  <script src="assets/vendors/wowjs/wow.min.js"></script> 
+  <script src="assets/vendors/skrollr.min.js"></script> 
+  <script src="assets/vendors/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script> 
+  <script src="assets/vendors/magnific-popup/jquery.magnific-popup.min.js"></script> 
+  <script src="assets/vendors/jquery.mb.YTPlayer/jquery.mb.vimeo_player.min.js"></script> 
+  <!-- Own script--> 
+  <script src="assets/js/main.js"></script> 
+  <script src="assets/js/shortcode.js"></script>   
+ </body>
+</html>
